332e6eaca445e685640eba416ff807ce259dff80
Nanfive <wipadathawattanawut@gmail.com>
2022-09-06 22:26:06 +0700

    lab complete

---
+++ react_/my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello My Frined</h1>
+      <p>Friend List</p>
+      <ul>
+        <li>John</li>
+        <li>Jack</li>
+        <li>Jane</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
